c964b3f3696f230fe7ab871387a83711be6138cd
RaghavBatra <tb698@cornell.edu>
2016-11-26 19:46:29 -0500

    added php includes

---
+++ includes/header.php
@@ -0,0 +1,17 @@
+   <div id="global_nav">
+        <ul>
+            <li><a href=#>Home</a></li>
+            <li><a href=#>Event Schedule</a></li>
+            <li><a href=#>Vendors</a></li>
+            <li><a href=#>Directions</a></li>
+            <li><a href=#>Photo Gallery</a></li>
+            <li><a href=#>Contact Us</a></li>
+        </ul>
+    </div>
+    <!-- end of the navigation bar -->
+
+    <div id="top">
+        <img src="images/apple.jpg" alt="apple banner image">
+        <!--  Image credits: http://unicoicounty.org/apple-festival/ -->
+    </div>
+    <!-- end of the topic banner -->
---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="utf-8">
+    <title>Ithaca Apple Fest</title>
+    <link rel="stylesheet" href="css/site.css">
+    <link href="https://fonts.googleapis.com/css?family=Amatic+SC" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="js/change_logo.js"></script>
+    </head>
+
+<body>
+
+    <?php 
+        include 'includes/header.php';
+        ?>
+
+    <div id="content">
+        <h2> Join us for the 34th Annual Great Downtown Ithaca Apple Harvest Festival! </h2>
+        <h3>Date: September 30, 2017 - October 2, 2017 </h3>
+        <div id="row1">
+            <a href = #><img src="images/photogallery.jpg" class="photogallery" alt="photo gallery image"></a>
+            <p class= "vendor"><b>Over 100 vendors will be selling unique apple varieties:</b> hot apple cider donuts,
+              kettle corn, local farm fresh baked goods and a multitude of around the world specialties </p>
+            <img src="images/vendors.jpg" class="times" alt="event times">
+        </div>
+
+        <div id="nextcontentsection">
+            <h4>Come for... </p>
+            <p class = "overview">Craft Show, Live Music, Street Performers, Farmers Market, Food Vendors</p>
+            <p class = "apples"> ...AND LOTS OF APPLES </p>
+          </div>
+    </div>
+    
+    <?php 
+        include 'includes/footer.php';
+        ?>
+
+
+</body>
+</html>

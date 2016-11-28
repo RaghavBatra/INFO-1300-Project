4a2d98a06f93988c44c91958e9fcccd0dc9498ac
Vivian <vivian@dhcp-mcfad-1216.redrover.cornell.edu>
2016-11-28 18:17:39 -0500

    photo

---
+++ photogallery.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="utf-8">
+    <title>Ithaca Apple Fest</title>
+    <link rel="stylesheet" href="css/site.css">
+    <link href="https://fonts.googleapis.com/css?family=Amatic+SC" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="js/change_logo.js"></script>
+    </head>
+
+<body>
+
+    <?php 
+        include 'includes/header.php';
+        ?>
+
+    <div class="img">
+        <a target="_blank" href="images/photo1.jpg">
+        <img src="images/photo1.jpg" alt="Apple Fest sign" width="600" height="400">
+        <!--photo from https://c2.staticflickr.com/6/5751/22027344875_aa950b1b1a_b.jpg -->
+        </a>
+    </div>
+    
+    <div class="img">
+        <a target="_blank" href="images/photo2.jpg">
+        <img src="images/photo2.jpg" alt="Performance" width="600" height="400">
+        <!-- photo from https://c1.staticflickr.com/1/702/22037228081_bb262c0a2d_b.jpg -->
+        </a>
+    </div>
+
+    <div class="img">
+        <a target="_blank" href="images/photo3.jpg">
+        <img src="images/photo3.jpg" alt="Toys" width="600" height="400">
+        <!-- photo from https://c1.staticflickr.com/1/658/21404547274_a6b776f7c5_b.jpg -->
+        </a>
+    </div>
+
+    <div class="img">
+        <a target="_blank" href="images/photo4.jpg">
+        <img src="images/photo4.jpg" alt="Bags of apples" width="600" height="400">
+        <!-- photo from https://c2.staticflickr.com/6/5767/21839482248_f3d9157b68_b.jpg -->
+        </a>
+    </div>
+
+    <div class="img">
+        <a target="_blank" href="images/photo5.jpg">
+        <img src="images/photo5.jpg" alt="Crowd at Apple Fest" width="600" height="400">
+        <!-- https://c2.staticflickr.com/6/5689/22015033122_0f6407f424_b.jpg -->
+        </a>
+    </div>
+
+    <div class="img">
+        <a target="_blank" href="images/photo6.jpg">
+        <img src="images/photo6.jpg" alt="hot cider store" width="600" height="400">
+        <!-- photo from https://c2.staticflickr.com/6/5731/22001136566_c3dab9aaec_b.jpg -->
+        </a>
+    </div>
+
+
+</div>
+    
+    <?php 
+        include 'includes/footer.php';
+        ?>
+
+
+</body>
+</html>

43ece01d9ac8d23cbb00747828d421de1e20bd4e
Chelsea Luo <cl773@cornell.edu>
2016-11-30 12:12:59 -0500

    Add files via upload
    event schedule page

---
+++ eventschedule.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="utf-8">
+    <title>Ithaca Apple Fest</title>
+    <link rel="stylesheet" href="css/site.css">
+    <link href="https://fonts.googleapis.com/css?family=Amatic+SC" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="js/change_logo.js"></script>
+    </head>
+
+<body>
+    <?php 
+        include 'includes/header.php';
+        ?>
+    <h3> 2016 Main Stage Schedule </h3>
+    <div id="scheduler">
+        <img src="images/event.jpg" alt="schedule" width="1100">
+        <! -- image created by Chelsea through Pages -->
+        
+    
+    
+    <?php 
+        include 'includes/footer.php';
+        ?>
+
+</body>
+</html>

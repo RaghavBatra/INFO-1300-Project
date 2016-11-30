526efae3a95d4ffcae0950b240d41f2eb9664b19
RaghavBatra <tb698@cornell.edu>
2016-11-30 17:10:45 -0500

    updated contactpage

--- contactpage.php
+++ contactpage.php
@@ -1,18 +1,3 @@
-
-            <!-- <p>The Downtown Ithaca Alliance office is located in the upper atrium of Center Ithaca at 171 E. State St.</p>
-            <p>The Downtown Ithaca Alliance mailing address is:
-            171 E. State St. PMB #136
-            Center Ithaca
-            Ithaca, NY 14850
-            USA </p>
-
-            <p>Phone: 607-277-8679</p>
-            <p>Fax: 607-277-8691</p>
-
-            <p>Email: info@downtownithaca.com</p> -->
-
-
-
 <!DOCTYPE html>
 <html>
     <head>
@@ -28,39 +13,46 @@
 
     <?php
         include 'includes/header.php';
-        ?>
+    ?>
+    
     <div id= "mainbody">
         <h5>Contact Us</h5>
-      <div id="maincontent">
-      <div id = "phone_address">
-          <img src="images/phone-icon.png" class= "icons"alt="phone icon">
-          <!-- Image Credits: http://icons.iconarchive.com/icons/graphicloads/100-flat/256/phone-icon.png-->
-          <p class="contactstyle"><b>Phone</b><br> 607-277-8679</br></p>
-          <p class="contactstyle"><b>Fax</b><br> 607-277-8691</br></p>
+        
+        <div id = "maincontent">
+            
+            <div id = "phone_address">
+              <img src="images/phone-icon.png" class= "icons"alt="phone icon">
+              <!-- Image credits: http://icons.iconarchive.com/icons/graphicloads/100-flat/256/phone-icon.png-->
+              <p class="contactstyle"><b>Phone</b> 607-277-8679</p>
+              <p class="contactstyle"><b>Fax</b> 607-277-8691</p>
+            </div>
+
+            <div id = "email_address">
+                <img src="images/email-icon.png" class ="icons" alt="email icon">
+                <!-- Image credits: http://racetimingsolutions.co.uk/wp-content/uploads/2010/07/email-icon-orange-1.png-->
+                <p class="contactstyle"><b>Email</b> info@downtownithaca.com </p>
+            </div>
+
+            <div id = "address_info">
+                <img src="images/location-icon.png" class="icons"alt="location icon">
+                 <!--Image credits: http://depo8.com/wp-content/themes/avid/images/location-icon.png-->
+                <p class = "contactstyle"><b>The Downtown Ithaca Alliance Office</b>
+                Upper Atrium of Center Ithaca at 171 E. State St.</p>
+                <p class= "contactstyle"><b>The Downtown Ithaca Alliance Mailing Address</b>
+                  171 E. State St. PMB #136
+                  Center Ithaca
+                  Ithaca, NY 14850
+                  USA</p>
+
         </div>
-
-      <div id = "email_address">
-            <img src="images/email-icon.png" class ="icons" alt="email icon">
-            <!-- Image Credits: http://racetimingsolutions.co.uk/wp-content/uploads/2010/07/email-icon-orange-1.png-->
-            <p class="contactstyle"><b>Email</b><br> info@downtownithaca.com</br></p>
-      </div>
-
-    <div id="address_info">
-          <img src="images/location-icon.png" class="icons"alt="location icon">
-          <!--Image Credits: http://depo8.com/wp-content/themes/avid/images/location-icon.png-->
-        <p class = "contactstyle"><b>The Downtown Ithaca Alliance Office</b><br> Upper Atrium of Center Ithaca at 171 E. State St.</br></p>
-        <p class= "contactstyle"><b>The Downtown Ithaca Alliance Mailing Address</b><br>
-          171 E. State St. PMB #136<br>
-          Center Ithaca<br>
-          Ithaca, NY 14850<br>
-          USA<br></p>
-
+    
     </div>
-  </div>
-  </div>
+    
+</div>
+    
     <?php
         include 'includes/footer.php';
-        ?>
+    ?>
 
 
 </body>

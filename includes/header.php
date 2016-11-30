7002939a8f06d0a5d91c63960510833d4a523954
RaghavBatra <tb698@cornell.edu>
2016-11-30 17:13:00 -0500

    updated header.php to include links to other pages

--- includes/header.php
+++ includes/header.php
@@ -1,9 +1,9 @@
    <div id="global_nav">
         <ul>
             <li><a href="index.php">Home</a></li>
-            <li><a href=#>Event Schedule</a></li>
+            <li><a href="eventschedule.php">Event Schedule</a></li>
             <li><a href=#>Vendors</a></li>
-            <li><a href=#>Directions</a></li>
+            <li><a href="directions.php">Directions</a></li>
             <li><a href="photogallery.php">Photo Gallery</a></li>
             <li><a href="contactpage.php">Contact Us</a></li>
         </ul>

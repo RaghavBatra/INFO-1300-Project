9d9037157652f2c3279ba62a8734c55332096e89
Vivian <vivian@dhcp-ccc-8811.redrover.cornell.edu>
2016-11-30 16:04:10 -0500

    photos

--- photogallery.php
+++ photogallery.php
@@ -56,7 +56,49 @@
         <!-- photo from https://c2.staticflickr.com/6/5731/22001136566_c3dab9aaec_b.jpg -->
         </a>
     </div>
-    
+
+    <div class="img">
+        <a target="_blank" href="images/photo7.jpg">
+        <img src="images/photo7.jpg" alt="craft tiles">
+        <!-- photo from http://c1.staticflickr.com/1/689/21839487448_a236d3f373_c.jpg-->
+        </a>
+    </div>
+
+    <div class="img">
+        <a target="_blank" href="images/photo8.jpg">
+        <img src="images/photo8.jpg" alt="cider and brussels store">
+        <!-- photo from https://c1.staticflickr.com/1/661/21406193973_5c116db026_b.jpg -->
+        </a>
+    </div>
+
+    <div class="img">
+        <a target="_blank" href="images/photo9.jpg">
+        <img src="images/photo9.jpg" alt="toy booth">
+        <!-- photo from https://c2.staticflickr.com/6/5597/15266693508_0978216c3c_b.jpg -->
+        </a>
+    </div>
+
+    <div class="img">
+        <a target="_blank" href="images/photo10.jpg">
+        <img src="images/photo10.jpg" alt="ferris wheel">
+        <!-- photo from https://c2.staticflickr.com/4/3930/15266598780_104b08c412_b.jpg -->
+        </a>
+    </div>
+
+    <div class="img">
+        <a target="_blank" href="images/photo11.jpg">
+        <img src="images/photo11.jpg" alt="apples">
+        <!-- photo from https://c2.staticflickr.com/4/3750/10397073603_44526f0c2e_b.jpg -->
+        </a>
+    </div>
+
+    <div class="img">
+        <a target="_blank" href="images/photo12.jpg">
+        <img src="images/photo12.jpg" alt="hot cider store">
+        <!-- photo from https://c2.staticflickr.com/4/3927/15266595120_b32524ef48_b.jpg-->
+        </a>
+    </div>
+
     <?php 
         include 'includes/footer.php';
         ?>

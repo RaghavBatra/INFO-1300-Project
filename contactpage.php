1c42d8912f39833f2df3f5c75618b881010712bf
Rona Chang <ronachang@dhcp-mcfad-6830.eduroam.cornell.edu>
2016-11-30 22:31:26 -0500

    changed contacts page and css

--- contactpage.php
+++ contactpage.php
@@ -14,42 +14,43 @@
     <?php
         include 'includes/header.php';
     ?>
-    
+
     <div id= "mainbody">
         <h5>Contact Us</h5>
-        
+
         <div id = "maincontent">
-            
+          <div id = "address_info">
+              <img src="images/location-icon.png" class="icons"alt="location icon">
+               <!--Image credits: http://depo8.com/wp-content/themes/avid/images/location-icon.png-->
+              <p class = "contactstyle"><b>The Downtown Ithaca Alliance Office<br></b>
+              Upper Atrium of Center Ithaca at<br>
+              171 E. State St.</p>
+              <p class= "contactstyle"><b>The Downtown Ithaca Alliance Mailing Address</b><br>
+                171 E. State St. PMB #136<br>
+                Center Ithaca<br>
+                Ithaca, NY 14850<br>
+                USA</p>
+        </div>
+
             <div id = "phone_address">
               <img src="images/phone-icon.png" class= "icons"alt="phone icon">
               <!-- Image credits: http://icons.iconarchive.com/icons/graphicloads/100-flat/256/phone-icon.png-->
-              <p class="contactstyle"><b>Phone</b> 607-277-8679</p>
-              <p class="contactstyle"><b>Fax</b> 607-277-8691</p>
+              <p class="contactstyle"><b>Phone</b><br> 607-277-8679</p>
+              <p class="contactstyle"><b>Fax</b><br> 607-277-8691</p>
             </div>
 
             <div id = "email_address">
                 <img src="images/email-icon.png" class ="icons" alt="email icon">
                 <!-- Image credits: http://racetimingsolutions.co.uk/wp-content/uploads/2010/07/email-icon-orange-1.png-->
-                <p class="contactstyle"><b>Email</b> info@downtownithaca.com </p>
+                <p class="contactstyle"><b>Email</b><br>info@downtownithaca.com </p>
+                <img src="images/downtown_ithaca.png" id="downtownithaca" alt="Downtown Ithaca">
+                <!--Image credits: http://www.downtownithaca.com/local/file_upload/images/Screen%20Shot%202014-01-30%20at%208_54_03%20PM.png-->
             </div>
 
-            <div id = "address_info">
-                <img src="images/location-icon.png" class="icons"alt="location icon">
-                 <!--Image credits: http://depo8.com/wp-content/themes/avid/images/location-icon.png-->
-                <p class = "contactstyle"><b>The Downtown Ithaca Alliance Office</b>
-                Upper Atrium of Center Ithaca at 171 E. State St.</p>
-                <p class= "contactstyle"><b>The Downtown Ithaca Alliance Mailing Address</b>
-                  171 E. State St. PMB #136
-                  Center Ithaca
-                  Ithaca, NY 14850
-                  USA</p>
-
-        </div>
-    
     </div>
-    
+
 </div>
-    
+
     <?php
         include 'includes/footer.php';
     ?>

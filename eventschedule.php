72953d44f255e9f994daf316dffdf84249613e11
Chelsea Luo <cl773@cornell.edu>
2016-12-01 20:51:45 -0500

    Add files via upload

--- eventschedule.php
+++ eventschedule.php
@@ -16,10 +16,92 @@
     ?>
     <h3> 2016 Main Stage Schedule </h3>
     
-    <div id="scheduler">
-        <img src="images/event.jpg" alt="schedule" width="1100">
-        <! -- image created by Chelsea through Pages -->
-        
+    <div id="sat">
+        <table id=Saturday>
+            <tr>
+                <th>Time</th>
+                <th>Event</th>
+            </tr>
+            <tr>
+                <th>10:00 - 10:45AM</th>
+                <th>Punk Hands </th>
+            </tr>
+            <tr>
+                <th>11:00 - 11:45AM</th>
+                <th>Danza Romani</th>
+            </tr>
+            <tr>
+                <th>12:00 - 12:45PM</th>
+                <th>Vitamin L</th>
+            </tr>
+            <tr>
+                <th>1:00 - 1:45PM</th>
+                <th>Riley Owens</th>
+            </tr>
+            <tr>
+                <th>1:45 - 2PM</th>
+                <th>GiveAways</th>
+            </tr>
+            <tr>
+                <th>2:00 - 2:45PM</th>
+                <th>SUNNY Weather</th>
+            </tr>
+            <tr>
+                <th>3:00 - 3:45PM</th>
+                <th>Eems</th>
+            </tr>
+            <tr>
+                <th>4:00 - 4:45PM</th>
+                <th>Alter</th>
+            </tr>
+        </table>
+    </div>
+    <div id="sun">
+        <table id=Sunday>
+            <tr>
+                <th>Time</th>
+                <th>Event</th>
+            </tr>
+            <tr>
+                <th>10:00 - 10:45AM</th>
+                <th>IC Voicestream, Ithacapella， Premium Band</th>
+            </tr>
+            <tr>
+                <th>11:00 - 11:45AM</th>
+                <th>Sixteen Feathers</th>
+            </tr>
+            <tr>
+                <th>12:00 - 12:45PM</th>
+                <th>June w/ Chandani</th>
+            </tr>
+            <tr>
+                <th>1:00 - 1:45PM</th>
+                <th>Mary Lorson</th>
+            </tr>
+            <tr>
+                <th>2:00 - 3:00PM</th>
+                <th>Dapper Dan</th>
+            </tr>
+            <tr>
+                <th>3:15 - 4:15PM</th>
+                <th>Radio London</th>
+            </tr>
+            <tr>
+                <th>4:30 - 5:00PM</th>
+                <th>Mirage Belly Dancers</th>
+            </tr>
+            <tr>
+                <th>5:15 - 6:15PM</th>
+                <th>NEO Project</th>
+            </tr>
+            <tr>
+                <th>5:00 - 6:00PM</th>
+                <th>Wallin' Brass Band</th>
+            </tr>
+        </table>
+    </div>
+            
+            
     
     <?php 
         include 'includes/footer.php';

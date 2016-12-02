385c946e429638367e7ec2918f972b19ffdd5539
Vivian <vivian@dhcp-mcfad-4145.redrover.cornell.edu>
2016-12-01 23:00:14 -0500

    styling

--- photogallery.php
+++ photogallery.php
@@ -15,25 +15,29 @@
     <?php 
         include 'includes/header.php';
     ?>
-    <p> 
-    <button onclick="previous()">Previous</button> 
-    <button onclick="next()">Next</button> 
-    </p>
+    
+    <div id = "slides">
 
-    <!--photo 1 from https://c2.staticflickr.com/6/5751/22027344875_aa950b1b1a_b.jpg 
-     photo 2 from https://c1.staticflickr.com/1/702/22037228081_bb262c0a2d_b.jpg 
-     photo 3 from https://c1.staticflickr.com/1/658/21404547274_a6b776f7c5_b.jpg
-     photo 4 from https://c2.staticflickr.com/6/5767/21839482248_f3d9157b68_b.jpg
-     photo 5 from https://c2.staticflickr.com/6/5689/22015033122_0f6407f424_b.jpg
-     photo 6 from https://c2.staticflickr.com/6/5731/22001136566_c3dab9aaec_b.jpg
-     photo 7 from https://c1.staticflickr.com/1/661/21406193973_5c116db026_b.jpg
-     photo 8 from https://c2.staticflickr.com/6/5597/15266693508_0978216c3c_b.jpg
-     photo 9 from https://c2.staticflickr.com/4/3930/15266598780_104b08c412_b.jpg
-     photo 10 from https://c2.staticflickr.com/4/3750/10397073603_44526f0c2e_b.jpg
-     photo 11 from https://c2.staticflickr.com/4/3927/15266595120_b32524ef48_b.jpg
-     -->
-    <img id="slides" src="images/photo1.jpg" alt="apple fest photos">
+        <p> 
+        <button class = "button" onclick="previous()">Previous</button> 
+        <button class = "button" onclick="next()">Next</button> 
+        </p>
 
+        <!--photo 1 from https://c2.staticflickr.com/6/5751/22027344875_aa950b1b1a_b.jpg 
+         photo 2 from https://c1.staticflickr.com/1/702/22037228081_bb262c0a2d_b.jpg 
+         photo 3 from https://c1.staticflickr.com/1/658/21404547274_a6b776f7c5_b.jpg
+         photo 4 from https://c2.staticflickr.com/6/5767/21839482248_f3d9157b68_b.jpg
+         photo 5 from https://c2.staticflickr.com/6/5689/22015033122_0f6407f424_b.jpg
+         photo 6 from https://c2.staticflickr.com/6/5731/22001136566_c3dab9aaec_b.jpg
+         photo 7 from https://c1.staticflickr.com/1/661/21406193973_5c116db026_b.jpg
+         photo 8 from https://c2.staticflickr.com/6/5597/15266693508_0978216c3c_b.jpg
+         photo 9 from https://c2.staticflickr.com/4/3930/15266598780_104b08c412_b.jpg
+         photo 10 from https://c2.staticflickr.com/4/3750/10397073603_44526f0c2e_b.jpg
+         photo 11 from https://c2.staticflickr.com/4/3927/15266595120_b32524ef48_b.jpg
+         -->
+        <img id="slides" src="images/photo1.jpg" alt="apple fest photos">
+
+    </div>
 
    
     <?php 

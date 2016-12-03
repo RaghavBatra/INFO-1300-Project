f4e0fd94b1402c91d0e4245a8190a1119b056a59
Chelsea Luo <cl773@cornell.edu>
2016-12-03 05:08:01 -0500

    Add files via upload

---
+++ attractions.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="utf-8">
+    <title>Ithaca Apple Fest</title>
+    <link rel="stylesheet" href="css/site.css">
+    <link href="https://fonts.googleapis.com/css?family=Amatic+SC" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="js/change_logo.js"></script>
+    </head>
+
+<body>
+    <?php
+        include 'includes/header.php';
+    ?>
+
+    <div id="attractions">
+        <h3>Cheap Food</h3>
+        <img src="images/larger.jpg" alt="apple">
+        <h3>Live Concert</h3>
+        <img src="images/concerto.jpg" alt="apple fest photos">
+        <h3>Vendors</h3>
+        <img src="images/vendor.jpg" alt="apple fest photos">
+    <!-- Image credits :photo 1 from https://assets.dnainfo.com/photo/2016/9/1475171260-275192/larger.jpg
+             Image credits: photo 2 from http://www.ithaca.edu/depts/gallery_img/16036_full.jpg
+             Image credits: photo 3 from http://l7.alamy.com/zooms/0744ce822e77479e89cc5c714d5379cd/apples-for-sale-at-annual-apple-festival-on-the-commons-in-ithaca-bedbk3.jpg -->
+
+    </div>
+    
+    <?php
+        include 'includes/footer.php';
+    ?>
+    
+</body>
+</html>

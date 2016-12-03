99d5bbdde20f39f510f6fe781effa94d636e1009
Chelsea Luo <cl773@cornell.edu>
2016-12-03 05:07:13 -0500

    Add files via upload

--- includes/header.php
+++ includes/header.php
@@ -2,7 +2,7 @@
         <ul>
             <li><a href="index.php">Home</a></li>
             <li><a href="eventschedule.php">Event Schedule</a></li>
-            <li><a href=#>Vendors</a></li>
+            <li><a href="attractions.php">Attractions</a></li>
             <li><a href="directions.php">Directions</a></li>
             <li><a href="photogallery.php">Photo Gallery</a></li>
             <li><a href="contactpage.php">Contact Us</a></li>

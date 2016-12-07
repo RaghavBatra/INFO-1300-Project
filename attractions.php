39777f135ca9dd7c95f37726a6bf77abbb36a089
Chelsea Luo <cl773@cornell.edu>
2016-12-06 22:14:03 -0500

    Add files via upload

--- attractions.php
+++ attractions.php
@@ -21,12 +21,24 @@
         <img src="images/concerto.jpg" alt="apple fest photos">
         <h3>Vendors</h3>
         <img src="images/attractionpic.jpg" alt="apple fest photos">
-        <!-- Image credits :photo 1 from https://assets.dnainfo.com/photo/2016/9/1475171260-275192/larger.jpg
-        Image credits: photo 2 from http://www.ithaca.edu/depts/gallery_img/16036_full.jpg
-        Image credits: photo 3 from http://l7.alamy.com/zooms/0744ce822e77479e89cc5c714d5379cd/apples-for-sale-at-annual-apple-festival-on-the-commons-in-ithaca-bedbk3.jpg -->
+        <h3>Local Produce</h3>
+        <img src="images/fall2.jpg" alt="apple produces">
+    <!-- Image credits :photo 1 from https://assets.dnainfo.com/photo/2016/9/1475171260-275192/larger.jpg
+             Image credits: photo 2 from http://www.ithaca.edu/depts/gallery_img/16036_full.jpg
+             Image credits: photo 3 from http://l7.alamy.com/zooms/0744ce822e77479e89cc5c714d5379cd/apples-for-sale-at-annual-apple-festival-on-the-commons-in-ithaca-bedbk3.jpg
+             Image credits: photo 4 from http://www.visitmayberry.com/autumn-mount-airy-north-carolina-love-fall/-->
 
     </div>
-
+    <h2>Some Must Dos</h2>
+    <div id="mustsee">
+        <p>Ithaca Apple Fest is a good opportunity</p>
+        <p>to explore the local farmed goods,</p>
+        <p>to enjoy concerts and cheap foods,</p>
+        <p>to immerse in the local culture,</p>
+        <p>and most importantly, to have fun!</p>
+    </div>
+    <br>
+        
     <?php
         include 'includes/footer.php';
     ?>
